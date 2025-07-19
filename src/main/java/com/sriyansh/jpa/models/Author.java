666a83de3a590ecf923e412f9b8e5851cebbc0a6
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-07-19 14:48:26 +0530

    Table Creation concept and implementation with important annotations.

--- src/main/java/com/sriyansh/jpa/models/Author.java
+++ src/main/java/com/sriyansh/jpa/models/Author.java
@@ -1,24 +1,37 @@
 package com.sriyansh.jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
-@NoArgsConstructor
-@Entity
+@NoArgsConstructor // lombok annotation it generates no argument constructor automatically.
+@Entity         //tells spring boot that it is a entity
+@Table(name = "AUTHOR_TBL")
 public class Author {
+
     @Id
     @GeneratedValue
     private Integer id;
 
+    @Column(name = "first_name",length = 35) // length will restrict first name length to 35 or less than 35.
     private String firstName;
 
+    @Column(name="last_name")
     private String lastname;
 
+    @Column(unique = true, nullable = false) // specifies mapped columns only contains unique emails
     private String email;
 
+    @Column(nullable = false)
     private int age;
+
+    @Column(updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
+
 }
